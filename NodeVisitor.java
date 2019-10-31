8be6a72fa2e15cc8f23bcf2581b114d5cd6c523a
ngjain <54864607+ngjain@users.noreply.github.com>
2019-10-31 16:02:07 -0700

    Update NodeVisitor.java

--- NodeVisitor.java
+++ NodeVisitor.java
@@ -8,17 +8,17 @@ package hacs;
  */
 
 abstract public class NodeVisitor {
-
+//constructor
 	public NodeVisitor() {
 	}
-
+//constructor
 	public NodeVisitor(Object visitee) {
 	}
-
+//visit facde
 	abstract public void visitFacade(Facade facade);
-
+//visit course
 	abstract public void visitCourse(Course course);
-
+//visit assignment
 	abstract public void visitAssignment(Assignment assignment);
 
 }
