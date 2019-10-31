73c7849f6ccf011e4591ee3c7d9a6e15e3465f73
ngjain <54864607+ngjain@users.noreply.github.com>
2019-10-31 09:38:07 -0700

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -22,18 +22,18 @@ public class Assignment {
   protected Solution suggestsolution=new Solution();
 
 
-
+//constructor
   public Assignment() {
   }
-
+//setduedate for assign
   public void setDueDate(Date theDueDate){
     this.duedate = theDueDate;
   }
-
+//specifications about assign
   public void setAssSpec(String theSpec){
     this.assspec = theSpec;
   }
-
+//check if late
   public boolean isOverDue(){
     Date today;
     today = new Date();
@@ -44,59 +44,59 @@ public class Assignment {
       return false;
     }
   }
-
+//add a new solution for assign. imstance created
   public Solution addSolution(){
     Solution mySolution = new Solution();
     return mySolution;
   }
 
-  ////add the theSolution to the Solutionlist
+  ////add the theSolution to the Solutionlist. solution added to list
   public void addSolution(Solution theSolution)
   {
     thesolutionlist.add(theSolution);
   }
-
+//abstract to submit solution
   public void submitSolution(){
   }
-
+//display solution
   public SolutionList getSolutionList(){
 	  return thesolutionlist;
   }
 
-  /* return the solution of the give name
-  */
+ // return the solution of the give name
+  
   public Solution getSolution(String studentname)
   {
     SolutionIterator Iterator=(SolutionIterator)thesolutionlist.iterator();
     return (Solution)Iterator.next(studentname);
   }
-
+// get sug solution
   public Solution getSugSolution(){
     return suggestsolution;
   }
-
+//iterate through solition list
   public SolutionIterator getSolutionIterator()
   {
     SolutionIterator theSolutionIterator=new SolutionIterator(thesolutionlist);
     return theSolutionIterator;
   }
-
+// gives assignment name
   public String toString()
   {
     return assignmentname;
   }
-
+// get due date as a string. useful for printing
   public String getDueDateString()
   {
     DateFormat dateFormat=DateFormat.getDateInstance(DateFormat.SHORT);
     return  dateFormat.format(duedate);
   }
-
+//accept
   public void accept(NodeVisitor visitor)
   {
     visitor.visitAssignment(this);
   }
-
+//abstract
 public Object getAssSpec() {
 	// TODO Auto-generated method stub
 	return assspec;
