c2dd8534e7111fa6a4650ff6e1f2fa0be1b98fe4
ngjain <54864607+ngjain@users.noreply.github.com>
2019-11-01 14:38:08 -0700

    Update HacsTest.java

--- HacsTest.java
+++ HacsTest.java
@@ -1,5 +1,13 @@
 package hacs;
 
+/**
+ * Title:        HacsAssignmentTest
+ * Description:  SER515:Testing and reuse
+ * Company:      ASU
+ * @author Jain Nayan
+ * @version 1.0
+ */
+
 //import java.io.IOException;
 
 import org.
