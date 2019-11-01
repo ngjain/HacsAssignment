cf0417c97a4254dd67901b0114f758824879b436
ngjain <54864607+ngjain@users.noreply.github.com>
2019-11-01 13:40:18 -0700

    Update ReminderVisitorTest.java

--- ReminderVisitorTest.java
+++ ReminderVisitorTest.java
@@ -7,18 +7,18 @@ import java.util.Date;
 import org.junit.jupiter.api.Test;
 
 class ReminderVisitorTest {
-
+//contrucr
 	@Test
 	void testReminderVisitor() {
 		System.out.println("Initialized reminder visitor");
 	}
-
+//reminder visitor second constrctor
 	@Test
 	void testReminderVisitorReminder() {
 		ReminderVisitor reminderVisitor = new ReminderVisitor(new Reminder());
 		assertNotNull(reminderVisitor.getM_Reminder());
 	}
-
+// cheksassignment by initialisig it and checking if it's empty
 	@Test
 	void testVisitAssignment() {
 		ReminderVisitor reminderVisitor = new ReminderVisitor(new Reminder());
