75af085a9f37e4b63c5901f65e870c38b45b0a80
ngjain <54864607+ngjain@users.noreply.github.com>
2019-11-01 14:36:28 -0700

    Update ReminderTest.java

--- ReminderTest.java
+++ ReminderTest.java
@@ -2,6 +2,15 @@ package hacs;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
+
+/**
+ * Title:        HacsAssignmentTest
+ * Description:  SER515:Testing and reuse
+ * Company:      ASU
+ * @author Jain Nayan
+ * @version 1.0
+ */
+
 import org.junit.jupiter.api.Test;
 
 class ReminderTest {
