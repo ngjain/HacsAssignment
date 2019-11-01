2dc2aa0498f02c9251b14f75c72f93e7ccfd21fb
ngjain <54864607+ngjain@users.noreply.github.com>
2019-11-01 14:38:34 -0700

    Update CourseTest.java

--- CourseTest.java
+++ CourseTest.java
@@ -1,5 +1,13 @@
 package hacs;
 
+/**
+ * Title:        HacsAssignmentTest
+ * Description:  SER515:Testing and reuse
+ * Company:      ASU
+ * @author Jain Nayan
+ * @version 1.0
+ */
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
