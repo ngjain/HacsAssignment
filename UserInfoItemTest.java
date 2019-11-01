916414dd0ea9121d89449ad7b0fec1397b89bfe4
ngjain <54864607+ngjain@users.noreply.github.com>
2019-11-01 14:34:57 -0700

    Update UserInfoItemTest.java

--- UserInfoItemTest.java
+++ UserInfoItemTest.java
@@ -1,4 +1,15 @@
 package hacs;
+
+
+
+/**
+ * Title:        HacsAssignmentTest
+ * Description:  SER515:Testing and reuse
+ * Company:      ASU
+ * @author Jain Nayan
+ * @version 1.0
+ */
+
 //Nothing to test here as USERInFO item has been already tested earlier like 
 //theFacade.createUser(userinfoitem); line 36 Hacs.java
 //import static org.junit.jupiter.api.Assertions.*;
