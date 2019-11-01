60ae528b0836c3e8a4f4b2d72ee079adb5c4995a
ngjain <54864607+ngjain@users.noreply.github.com>
2019-11-01 14:06:09 -0700

    Update InstructorTest.java

--- InstructorTest.java
+++ InstructorTest.java
@@ -28,7 +28,7 @@ class InstructorTest {
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-// Checking that for a given course we get a coursemenu
+// Checking that for a given course we get a coursemenu. The functionality of course menu is given in couremenu test
 	@Test
 	final void testCreateCourseMenu() {
 		CourseMenu testcreatecouremenu = instructor.createCourseMenu( theCourse,  theLevel);
